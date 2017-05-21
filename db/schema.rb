7bc9a280d49a0781ae88634fecc2a047a14df122
Aidan Levy <aidanlevy03@gmail.com>
2017-05-21 14:06:54 -0700

    added contact model file

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170521204524) do
+ActiveRecord::Schema.define(version: 20170521205527) do
 
   create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.text   "comments"
   end
 
 end
