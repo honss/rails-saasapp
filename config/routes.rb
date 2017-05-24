226a4a50d64c4d71b0796de55587cd9a43fb522a
Aidan Levy <aidanlevy03@gmail.com>
2017-05-23 18:03:28 -0700

    added custom routes for bug + contact form

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get 'about', to:'pages#about'
   resources 'contacts'
   resources 'bugs'
+  get 'contact-us', to: 'contacts#new'
+  get 'bug-report', to:'bugs#new'
 end
