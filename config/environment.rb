860581278c2d1c0fba3d72957f5be642c33ad667
Aidan Levy <aidanlevy03@gmail.com>
2017-05-27 14:40:17 -0700

    one letter mistake

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ ActionMailer::Base.smtp_settings = {
 	:address => ENV['MAILGUN_SMTP_SERVER'],
 	:user_name => ENV['MAILGUN_SMTP_LOGIN'],
 	:password => ENV['MAILGUN_SMTP_PASSWORD'],
-	:domain = > 'obscure-sierra-40079.herokuapp.com',
+	:domain => 'obscure-sierra-40079.herokuapp.com',
 	:authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
